bfaefaba10eaab92f121b030e6026fc6118a9807
fqqdk <fqqdk1@gmail.com>
2015-10-02 10:44:49 +0200

    first test

--- test.php
+++ test.php
@@ -1,4 +1,19 @@
 <?php
 
-require_once('player.php');
-require_once('gamestate.php');
+require_once 'vendor/autoload.php';
+require_once 'player.php';
+require_once 'gamestate.php';
+
+class GameStateTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function firstTest()
+    {
+        $gameStateJson = file_get_contents('test.json');
+        $gameState = GameState::create(json_decode($gameStateJson, true));
+
+        $this->assertEquals(2, $gameState->activePlayers());
+    }
+}
