14ab0c6f3be3e10174abfecb613ec96c6942f48b
Gabor Balla <gabor.balla@emarsys.com>
2015-10-02 14:28:53 +0200

    make some logs

--- src/player.php
+++ src/player.php
@@ -19,20 +19,25 @@ class Player
         $detector = Detector::create(__DIR__ . '/../preflop.csv');
         $value = $detector->check($myHand);
 
+        $playerCount = count($gameState->getPlayers());
         if ($value == -1)
         {
-            if (count($gameState->getPlayers()) > 2)
+            if ($playerCount > 2)
             {
+                Logger::log('folding: ' . $playerCount . ' - ' . $myHand[0]->rankForStat() . ':' . $myHand[1]->rankForStat());
                 return $fold;
             }
+            Logger::log('allin: ' . $playerCount . ' - ' . $myHand[0]->rankForStat() . ':' . $myHand[1]->rankForStat());
             return $allIn;
         }
         elseif ($value < 8)
         {
+            Logger::log('raise: ' . $playerCount . ' - ' . $myHand[0]->rankForStat() . ':' . $myHand[1]->rankForStat());
             return $raise;
         }
         else
         {
+            Logger::log('call: ' . $playerCount . ' - ' . $myHand[0]->rankForStat() . ':' . $myHand[1]->rankForStat());
             return $call;
         }
 
